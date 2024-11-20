4222ae7fa28480a354b677acc16aef92ba88ed62
pankaj-maker <Pankaj14321@protonmail.com>
2024-11-20 07:40:28 +0000

    resume updated

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# wakati
+# Wakati
+
+Wakati is a simple API to calculate the reading speed of text.
+
+## Usage
+
+### Check the status
+
+```bash
+https://wakati.wofad91300.workers.dev/status
+```
+
+### GET
+
+You need to send the text and the words per minute (wpm optional) as query parameters.
+
+```
+https://wakati.wofad91300.workers.dev/?text=Hello%20World&wpm=238
+```
+
+### POST
+
+You need to send the text and the words per minute (wpm optional) as JSON.
+
+```
+https://wakati.wofad91300.workers.dev/
+
+{
+  "text": "Hello World",
+  "wpm": 238
+}
+
+```
+
+## Develop
+
+```bash
+npm install
+npm run dev
+```
+
+And to deploy
+
+```bash
+npm run deploy
+
+```
+
+To set the environment variables
+
+```bash
+export CLOUDFLARE_API_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+// Middlewares
+app.use(logger()); // Enable logger
+app.use(cors());
+
+const defaultWPM = 238;
+
+function calculateSpeed(
+  text: string,
+  wpm: number
+): { wpm: number; seconds: number; minutes: number; wordsCount: number } {
+  const wordsCount = text.split(/\s+/).length;
+  const seconds: number = (wordsCount / wpm) * 60;
+  const minutes: number = seconds / 60;
+  return {
+    wpm,
+    seconds: Number(seconds.toFixed(2)),
+    minutes: Number(minutes.toFixed(2)),
+    wordsCount,
+  };
+}
+
+// MARK: Routes
+app.get("/status", (c) => {
+  return c.json({ message: "API is active 🔥" });
+});
+
+app.get("/", (c) => {
+  const text = c.req.query("text");
+  const wpm = c.req.query("wpm");
+
+  if (!text) {
+    return c.json({ message: "Please provide text" }, 400);
+  }
+
+  const result = calculateSpeed(text, wpm ? Number(wpm) : defaultWPM);
+
+  return c.json(result);
+});
+
+app.post("/", async (c) => {
+  const { text, wpm } = await c.req.json();
+
+  if (!text) {
+    return c.json({ message: "Please provide text" }, 400);
+  }
+
+  const result = calculateSpeed(text, Number(wpm) || defaultWPM);
+
+  return c.json(result);
+});
+
+export default app;
